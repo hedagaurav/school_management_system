02bf5837d784ae3c04b02e6a32873edaaaeb6521
Gaurav Heda <hedagaurav93@gmail.com>
2020-11-17 16:36:44 +0530

    changed

--- app/Views/home.php
+++ app/Views/home.php
@@ -1,6 +1,6 @@
+<?= $this->extend('layouts/app.php'); ?>
 
-
-
+<?= $this->section('main');?>
 	<p>intro and documentation about the app</p>
 
   <p>Details About School</p>
@@ -8,3 +8,4 @@
   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aliquid architecto dignissimos doloremque eveniet, non nulla pariatur quisquam rem vitae! Aut ea eaque excepturi magni nulla quidem, sit unde. Aperiam.</p>
 
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aliquid architecto dignissimos doloremque eveniet, non nulla pariatur quisquam rem vitae! Aut ea eaque excepturi magni nulla quidem, sit unde. Aperiam.</p>
+<?= $this->endSection();?>
